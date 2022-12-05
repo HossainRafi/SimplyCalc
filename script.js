2d0793bc11dd136d27dc763653dc27b4a77029c4
HossainRafi <hossainrafi71@gmail.com>
2022-12-05 18:55:45 +0600

    get value from keyboard nymber

--- script.js
+++ script.js
@@ -96,3 +96,33 @@ clearLastEl.addEventListener("click", () => {
   display2El.innerText = "";
   dis2Num = "";
 });
+
+
+// Get value from keyboard
+window.addEventListener("keydown", (e) => {
+  if (
+    e.key === "0" ||
+    e.key === "1" ||
+    e.key === "2" ||
+    e.key === "3" ||
+    e.key === "4" ||
+    e.key === "5" ||
+    e.key === "6" ||
+    e.key === "7" ||
+    e.key === "8" ||
+    e.key === "9" ||
+    e.key === "."
+  ) {
+    clickButtonEl(e.key);
+    // console.log(e.key)
+  }
+});
+
+// Number buttons
+function clickButtonEl(key) {
+  numbersEl.forEach((button) => {
+    if (button.innerText === key) {
+      button.click();
+    }
+  });
+}
